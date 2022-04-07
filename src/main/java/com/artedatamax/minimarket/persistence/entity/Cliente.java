50e71f29131260ada7194794a758d31abc191acc
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2022-04-07 16:31:50 -0300

    Added Entity Cliente

---
+++ src/main/java/com/artedatamax/minimarket/persistence/entity/Cliente.java
@@ -0,0 +1,27 @@
+package com.artedatamax.minimarket.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+@Getter
+@Setter
+public class Cliente {
+
+    @Id
+    private String id;
+
+    private String nombre;
+    private String apellidos;
+    private Long celular;
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+}
