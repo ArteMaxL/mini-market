6ee4e45e8fd1a4b7925230d2c9ec8c3b2cb44cbc
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2022-04-07 23:02:17 -0300

    Added Producto crud methods

--- src/main/java/com/artedatamax/minimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/artedatamax/minimarket/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,12 @@ package com.artedatamax.minimarket.persistence.crud;
 import com.artedatamax.minimarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
