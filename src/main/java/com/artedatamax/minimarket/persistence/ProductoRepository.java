b4f8e03dd00f34020ec05f0e6cfaeacca5dbdf31
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2022-04-13 13:34:55 -0300

    Minor changes ProductoRepository

--- src/main/java/com/artedatamax/minimarket/persistence/ProductoRepository.java
+++ src/main/java/com/artedatamax/minimarket/persistence/ProductoRepository.java
@@ -1,19 +1,46 @@
 package com.artedatamax.minimarket.persistence;
 
+import com.artedatamax.minimarket.domain.Product;
+import com.artedatamax.minimarket.domain.repository.ProductRepository;
 import com.artedatamax.minimarket.persistence.crud.ProductoCrudRepository;
 import com.artedatamax.minimarket.persistence.entity.Producto;
+import com.artedatamax.minimarket.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
+
+    private ProductMapper mapper;
 
     private ProductoCrudRepository productoCrudRepository;
 
-    public List<Producto> getAll(){
-        return (List<Producto>) productoCrudRepository.findAll();
+    public List<Product> getAll(){
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+
+    }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProduct(int quantity) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Product save(Product product) {
+        return null;
     }
 
     public List<Producto> getByCategoria(int idCategoria){
